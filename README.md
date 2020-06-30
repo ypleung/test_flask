5694fcf442ed82b376b692b956a88ada067fbd3e
ypleung <yuenpingleung@gmail.com>
2020-06-30 10:33:05 -0400

    Added in a new feature.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 1.0 in development
-
+2.0 new feature
--- main.py
+++ main.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_www():
+    msg = "Hello World!"
+    msg = msg + "\n" + "Adding some features."
     return "Hello World!" 
